74aecd0691d1e739b294850fc0b17c5a693a7b1d
Tatu Nordström <tatu.nordstrom@solita.fi>
2024-01-23 10:09:11 +0200

    Initial commit

---
+++ src/main/java/com/devacademy/discussionforum/DiscussionForumApplication.java
@@ -0,0 +1,13 @@
+package com.devacademy.discussionforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devacademy/discussionforum/DiscussionForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devacademy.discussionforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
