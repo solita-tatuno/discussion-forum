995aaf4ec0c3ad94428a1989ffee784e1e574155
Tatu Nordström <tatu.nordstrom@solita.fi>
2024-03-19 13:56:14 +0200

    simplify topicrow

--- client/src/components/TopicActions.tsx
+++ client/src/components/TopicActions.tsx
@@ -1,17 +1,46 @@
-import { Dispatch } from "react";
+import { useState } from "react";
 import useCurrentUser from "../hooks/useCurrentUser.ts";
+import Modal from "./Modal.tsx";
+import useUpdateTopic from "../hooks/useUpdateTopic.ts";
+import useDeleteTopic from "../hooks/useDeleteTopic.ts";
+import { Topic } from "../types";
 
-interface EditActionsProps {
-  setEditModalOpen: Dispatch<boolean>;
-  handleDelete: () => void;
+interface Props {
+  topic: Topic;
 }
 
-function TopicActions({ setEditModalOpen, handleDelete }: EditActionsProps) {
+function TopicActions({ topic }: Props) {
   const { currentUser } = useCurrentUser();
+  const [editModalOpen, setEditModalOpen] = useState<boolean>(false);
+  const [newTopicName, setNewTopicName] = useState<string>(topic.name);
+  const { updateTopic } = useUpdateTopic();
+  const { deleteTopic } = useDeleteTopic();
 
   if (!currentUser?.isAdmin) {
     return null;
   }
+
+  const handleUpdateTopic = () => {
+    updateTopic({ id: topic.id, name: newTopicName });
+    setEditModalOpen(false);
+  };
+
+  const handleDelete = () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this topic?",
+    );
+
+    if (!confirm) {
+      return;
+    }
+
+    deleteTopic(topic.id);
+  };
+
+  const handleEditModalClose = () => {
+    setEditModalOpen(false);
+    setNewTopicName(topic.name);
+  };
 
   return (
     <div className="absolute right-1 top-0 hidden gap-3 group-hover:flex">
@@ -27,6 +56,24 @@ function TopicActions({ setEditModalOpen, handleDelete }: EditActionsProps) {
       >
         Delete
       </button>
+
+      <Modal isOpen={editModalOpen} onClose={handleEditModalClose}>
+        <div className="flex flex-col gap-3 text-center">
+          <h2 className="text-2xl font-semibold">Update Topic</h2>
+          <input
+            type="text"
+            placeholder="New topic name"
+            value={newTopicName}
+            onChange={(e) => setNewTopicName(e.target.value)}
+          />
+          <button
+            className="rounded-md bg-green-600 p-3"
+            onClick={handleUpdateTopic}
+          >
+            Update
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
--- client/src/components/TopicRow.tsx
+++ client/src/components/TopicRow.tsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom";
 import { Topic } from "../types";
-import { useState } from "react";
 import TopicActions from "./TopicActions.tsx";
-import useUpdateTopic from "../hooks/useUpdateTopic.ts";
-import Modal from "./Modal.tsx";
-import useDeleteTopic from "../hooks/useDeleteTopic.ts";
 import TopicDetails from "./TopicDetails.tsx";
 
 interface Props {
@@ -12,33 +8,6 @@ interface Props {
 }
 
 function TopicRow({ topic }: Props) {
-  const [editModalOpen, setEditModalOpen] = useState<boolean>(false);
-  const [newTopicName, setNewTopicName] = useState<string>(topic.name);
-  const { updateTopic } = useUpdateTopic();
-  const { deleteTopic } = useDeleteTopic();
-
-  const handleUpdateTopic = () => {
-    updateTopic({ id: topic.id, name: newTopicName });
-    setEditModalOpen(false);
-  };
-
-  const handleDelete = () => {
-    const confirm = window.confirm(
-      "Are you sure you want to delete this topic?"
-    );
-
-    if (!confirm) {
-      return;
-    }
-
-    deleteTopic(topic.id);
-  };
-
-  const handleEditModalClose = () => {
-    setEditModalOpen(false);
-    setNewTopicName(topic.name);
-  };
-
   return (
     <li className="group relative">
       <Link
@@ -49,28 +18,7 @@ function TopicRow({ topic }: Props) {
         <TopicDetails topic={topic} />
       </Link>
 
-      <TopicActions
-        setEditModalOpen={setEditModalOpen}
-        handleDelete={handleDelete}
-      />
-
-      <Modal isOpen={editModalOpen} onClose={handleEditModalClose}>
-        <div className="flex flex-col gap-3 text-center">
-          <h2 className="text-2xl font-semibold">Update Topic</h2>
-          <input
-            type="text"
-            placeholder="New topic name"
-            value={newTopicName}
-            onChange={(e) => setNewTopicName(e.target.value)}
-          />
-          <button
-            className="rounded-md bg-green-600 p-3"
-            onClick={handleUpdateTopic}
-          >
-            Update
-          </button>
-        </div>
-      </Modal>
+      <TopicActions topic={topic} />
     </li>
   );
 }
