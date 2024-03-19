c3e4ce1b57ce534e1d5f91cd7bbd982b14de71f2
Tatu Nordström <tatu.nordstrom@solita.fi>
2024-03-19 11:43:28 +0200

    refactor pagination tests

--- client/src/tests/Pagination.test.tsx
+++ client/src/tests/Pagination.test.tsx
@@ -5,12 +5,13 @@ import Pagination from "../components/Pagination";
 describe("Pagination tests", () => {
   test("renders correct page count", () => {
     render(
-      <Pagination itemCount={16} page={0} setPage={() => null} size={5} />
+      <Pagination itemCount={16} page={1} setPage={() => null} size={5} />,
     );
     expect(screen.getByText("1")).toBeDefined();
     expect(screen.getByText("2")).toBeDefined();
     expect(screen.getByText("3")).toBeDefined();
     expect(screen.getByText("4")).toBeDefined();
+    expect(screen.queryByText("5")).toBeNull();
   });
 
   test("set page is called with correct value when next link is clicked", async () => {
@@ -25,19 +26,28 @@ describe("Pagination tests", () => {
     expect(setPage).toHaveBeenCalledWith(3);
   });
 
-  test("renders correct range text", async () => {
-    const testCases = [
-      { page: 1, expectedText: "Showing 1 to 5 of 16 results" },
-      { page: 2, expectedText: "Showing 6 to 10 of 16 results" },
-      { page: 3, expectedText: "Showing 11 to 15 of 16 results" },
-      { page: 4, expectedText: "Showing 16 to 16 of 16 results" },
-    ];
+  test.each([
+    4,
+    2,
+    3,
+  ])("setPage is called with correct value when page number %s is clicked", (pageNumber) => {
+    const setPage = vi.fn();
 
-    testCases.forEach(({ page, expectedText }) => {
-      render(
-        <Pagination itemCount={16} page={page} setPage={() => null} size={5} />
-      );
-      expect(screen.getByText(expectedText)).toBeDefined();
-    });
+    render(<Pagination itemCount={16} page={1} setPage={setPage} size={5} />);
+
+    fireEvent.click(screen.getByText(pageNumber));
+    expect(setPage).toHaveBeenCalledWith(pageNumber);
+  });
+
+  test.each([
+    { page: 1, expectedText: "Showing 1 to 5 of 16 results" },
+    { page: 2, expectedText: "Showing 6 to 10 of 16 results" },
+    { page: 3, expectedText: "Showing 11 to 15 of 16 results" },
+    { page: 4, expectedText: "Showing 16 to 16 of 16 results" },
+  ])("renders correct range text for page $page", ({ page, expectedText }) => {
+    render(
+      <Pagination itemCount={16} page={page} setPage={() => null} size={5} />,
+    );
+    expect(screen.getByText(expectedText)).toBeDefined();
   });
 });
