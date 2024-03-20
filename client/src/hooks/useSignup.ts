bedcc1dd86b2cc2daefa420112e40fb28c232298
Tatu Nordström <tatu.nordstrom@solita.fi>
2024-03-20 13:08:31 +0200

    navigate to login on signup success

--- client/src/hooks/useSignup.ts
+++ client/src/hooks/useSignup.ts
@@ -1,14 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { UserCredentials } from "../types";
 import userService from "../services/users";
 import { toast } from "react-toastify";
 
 const useSignup = () => {
+  const navigate = useNavigate();
   const { mutate } = useMutation({
     mutationFn: (credentials: UserCredentials) =>
       userService.createOne(credentials),
     onError: (error) => {
       toast.error(error.message);
+    },
+    onSuccess: () => {
+      toast.success("Account created");
+      navigate("/login");
     },
   });
 
